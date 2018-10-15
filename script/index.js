842260eb3d8a715632c41ffad2158ffc57bbcde6
Taras <taras92.m@gmail.com>
2018-10-15 09:33:13 +0000

    Initial

--- script/index.js
+++ script/index.js
@@ -4,6 +4,16 @@ window.onload = ()=>{
   setInterval(()=>{
   const num = randomNumber();
   handle.style.left = 'calc(' + num + '% - 2rem)';
-  handle.innerHTML = num;
+  rotate(num);
   },2000)
+}
+
+function rotate(num){
+  const text = document.getElementsByClassName('wrap-text');
+  for(let i =0; i < text.length; i++){
+    text[i].classList.toggle('rotate');
+  }
+  setTimeout(()=>{
+  text[1].innerHTML = num;
+  }, 750)
 }
